effdd55c2bf33532492cc80ab30a3af9d0e34749
EDEN <EDBEK@YAHOO.COM>
2020-06-11 19:34:37 -0500

    Updated new artist for to be able to submit

--- src/views/ArtistsNew.vue
+++ src/views/ArtistsNew.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="container">
+      <div data-aos="fade-center"
+        data-aos-offset="200"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        data-aos-once="false"
+        class="artists-new rounded"> 
     <form v-on:submit.prevent="submit()">
       <h1>New Artist</h1>
       <ul>
         <li class="text-danger" v-for="error in errors">{{ error }}</li>
       </ul>
-    <div>
+    <div class="form-group">
       <label>Name:</label>
       <input type="text" class="form-control" v-model="name">
     </div>
-    <div>
+    <div class="form-group">
      <label>Description:</label>
       <input type="text" class="form-control" v-model="description">
     </div>
-     <div>
+    <div class="form-group">
       <label>Bio:</label> 
       <input type="text" class="form-control" v-model="bio">
     </div>
-     <div>
+    <div class="form-group">
       <label>Image:</label> 
       <input type="text" class="form-control" v-model="image">
     </div>
@@ -26,7 +34,22 @@
 
     </form>
     </div>
+    </div>
 </template>   
+
+<style>
+.artists-new {
+  width: 40%;
+  margin-bottom: 7em;
+  margin-top: 6em;
+  margin-left: 20em;
+  background-image: linear-gradient(rgba(53, 49, 49, 0.65), rgba(0, 0, 0, 0.65));
+  padding-left: 2em;
+  padding-right: 2em;
+  padding-top: 2em;
+  padding-bottom: 2em;
+}
+</style>
 
 <script>
 import axios from "axios";
@@ -51,8 +74,9 @@ export default {
         image: this.image,
       };
       var myVariable = 0;
+      console.log(params);
       axios
-        .post("/api/artists/new", params)
+        .post("/api/artists", params)
         .then(response => {
           this.$router.push("/artists");
         })
